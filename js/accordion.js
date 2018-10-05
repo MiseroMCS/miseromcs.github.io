30e81049cbe2723a13aa5c23089a9b6073981f64
MiseroMCS <miseromcs@gmail.com>
2018-10-04 22:00:34 -0400

    fixed up formatting and added a minor detail

--- js/accordion.js
+++ js/accordion.js
@@ -1,7 +1,7 @@
 document.querySelectorAll(".accordion").forEach((element) => {
     element.onclick = function(){
         label = this.querySelector("label").innerHTML;
-        contents = this.querySelector("p");
-        contents.className = (contents.className == "") ? "invisible" : "";
+        contents = this.querySelector(".contents");
+        contents.className = (contents.className == "contents") ? "contents invisible" : "contents";
     }
 });
